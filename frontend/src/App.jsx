37a541186cf1d776fa3ad78cab502042c584894e
Bhavya Singh <bhavisingh2000@gmail.com>
2025-11-05 17:57:25 +0530

    Initial commit: added project files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/home'
+import Footer from './Components/Footer/Footer'
+import Auth from './Components/Auth/Auth';
+import Services from './Components/Services/Services';
+import AdminLogin from './Components/AdminPanel/AdminLOgin';
+import Pets from './Components/Pets/Pets';
+import ProtectedRoute  from './Components/ProtectedRoute';
+import Contact from './Components/Contact/Contact';
+
+function App() {
+  
+
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Navbar />
+            <Home />
+            <Footer />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/services" element={
+         <ProtectedRoute>
+            <Navbar />
+            <Services />
+            <Footer />
+          </ProtectedRoute>
+      }/>
+     
+     <Route path="/pets" element={
+         <ProtectedRoute>
+            <Navbar />
+            <Pets />
+            <Footer />
+          </ProtectedRoute>
+      }/>
+      <Route path="/contact" element={
+         <ProtectedRoute>
+            <Navbar />
+            <Contact />
+            <Footer />
+          </ProtectedRoute>
+      }/>
+
+      <Route path="/auth" element={<Auth />} />
+
+        <Route 
+          path="/admin" 
+          element={<AdminLogin />} 
+        />
+    </Routes>
+  );
+}
+
+export default App
