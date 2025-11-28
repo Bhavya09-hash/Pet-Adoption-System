810ae312f28be06457c55395ca08f804a56ce340
Bhavya Singh <bhavisingh2000@gmail.com>
2025-11-28 16:30:24 +0530

    Fix import paths for Home and Navbar components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from './Components/Navbar/Navbar'
-import Home from './Components/Home/home'
-import Footer from './Components/Footer/Footer'
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/Home';
+import Footer from './Components/Footer/Footer';
 import Auth from './Components/Auth/Auth';
 import Services from './Components/Services/Services';
 import AdminLogin from './Components/AdminPanel/AdminLOgin';
