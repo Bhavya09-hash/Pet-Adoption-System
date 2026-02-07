3b004b14508998d94bda72ca208a541f9c9bef30
Bhavya Singh <bhavisingh2000@gmail.com>
2026-02-07 19:30:11 +0530

    Fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Home/Home';
 import Footer from './Components/Footer/Footer';
@@ -10,55 +6,59 @@ import Auth from './Components/Auth/Auth';
 import Services from './Components/Services/Services';
 import AdminLogin from './Components/AdminPanel/AdminLogin';
 import Pets from './Components/Pets/Pets';
-import ProtectedRoute  from './Components/ProtectedRoute';
+import ProtectedRoute from './Components/ProtectedRoute';
 import Contact from './Components/Contact/Contact';
 
 function App() {
-  
-
   return (
     <Routes>
-      <Route
-        path="/"
+      {/* Public route */}
+      <Route path="/auth" element={<Auth />} />
+      <Route path="/admin" element={<AdminLogin />} />
+
+      {/* Protected routes */}
+      <Route 
+        path="/" 
         element={
           <ProtectedRoute>
             <Navbar />
             <Home />
             <Footer />
           </ProtectedRoute>
-        }
+        } 
       />
-      <Route path="/services" element={
-         <ProtectedRoute>
+      <Route 
+        path="/services" 
+        element={
+          <ProtectedRoute>
             <Navbar />
             <Services />
             <Footer />
           </ProtectedRoute>
-      }/>
-     
-     <Route path="/pets" element={
-         <ProtectedRoute>
+        } 
+      />
+      <Route 
+        path="/pets" 
+        element={
+          <ProtectedRoute>
             <Navbar />
             <Pets />
             <Footer />
           </ProtectedRoute>
-      }/>
-      <Route path="/contact" element={
-         <ProtectedRoute>
+        } 
+      />
+      <Route 
+        path="/contact" 
+        element={
+          <ProtectedRoute>
             <Navbar />
             <Contact />
             <Footer />
           </ProtectedRoute>
-      }/>
-
-      <Route path="/auth" element={<Auth />} />
-
-        <Route 
-          path="/admin" 
-          element={<AdminLogin />} 
-        />
+        } 
+      />
     </Routes>
   );
 }
 
-export default App
+export default App;
